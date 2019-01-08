0ae3b7b1aca08b1bc92eda6870884b9356b5ff12
usuario <usuario@macbook-pro-de-macbook-2.local>
2019-01-08 16:02:08 -0600

    Se crean conexión entre los botones y controladores

--- Actividad3/ViewController.swift
+++ Actividad3/ViewController.swift
@@ -10,12 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtinpFactura: UITextField!
+    @IBOutlet weak var txtTotalPropina: UITextField!
+    var porcentaje:Double = 0;
+    @IBOutlet weak var txtTotalTotal: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Prueba de comit");
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func btnDiez(_ sender: UIButton) {
+        porcentaje = 0.10;
+    }
+    @IBAction func btnCinco(_ sender: UIButton) {
+        porcentaje = 0.05
+    }
+    
+    @IBAction func btnQuince(_ sender: UIButton) {
+        porcentaje = 0.15
+    }
+    @IBAction func btnVeinte(_ sender: Any) {
+        porcentaje = 0.20;
+    }
+    @IBAction func btnCalcular(_ sender: UIButton) {
+    }
 }
 
